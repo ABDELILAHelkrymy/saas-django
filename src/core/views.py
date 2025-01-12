60678aae2a13e323433c81c2f8ad64b536ac04f6
ABDELILAH EL KRYMY <63428583+ABDELILAHelkrymy@users.noreply.github.com>
2025-01-12 19:34:07 +0100

    fix issue

--- src/core/views.py
+++ src/core/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page_view(request, *args, **kwargs):
+    var = 'Hello World'
     context = {
         'title': 'home',
     }

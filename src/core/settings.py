5642590177a9d1165dd91161d3355980d3f9bb7c
ABDELILAH EL KRYMY <63428583+ABDELILAHelkrymy@users.noreply.github.com>
2025-01-15 19:27:24 +0100

    add dot env variables

--- src/core/settings.py
+++ src/core/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,11 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--$#h5c6sx254hao$@wta@_)ga-d1s3g%x3#40+nt(yv&w2(dj('
+SECRET_KEY = config('DJANGO_SECRET_KEY')
+# SECRET_KEY = 'django-insecure--$#h5c6sx254hao$@wta@_)ga-d1s3g%x3#40+nt(yv&w2(dj('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
+# DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
+
+DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
 print(f"DEBUG={DEBUG}")
+
 ALLOWED_HOSTS = [
     ".railway.app", # Allow domain and subdomains
 ]
